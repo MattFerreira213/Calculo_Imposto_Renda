fd61d2c4523ce9822b2533489a99d1f872d819b2
Mateus Ferreira <mateusanjos225@gmail.com>
2020-05-15 16:03:35 -0300

    Projeto finalizado, recebe os dados de um contribuinte e informa valor de imposto de renda a ser pago

---
+++ ImpostoRenda/Contribuintes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpostoRenda
+{
+    public class Contribuinte
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; private set; }
+        public int Dependentes { get; set; }
+        public double RendaBruta { get; set; }
+        public string Imposto { get; set; }
+
+        public Contribuinte(int id, string nome, string cpf, int dependentes, double rendaBruta)
+        {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+            Dependentes = dependentes;
+            RendaBruta = rendaBruta;
+        }
+
+        public double RendaLiquida()
+        {
+            return RendaBruta -= Dependentes * 0.05;
+        }
+
+        public string CalculoIR(double salarioMinimo)
+        {
+            if (RendaLiquida() < salarioMinimo * 2)
+            {
+                Imposto = "Isento";
+            }
+            else if (RendaLiquida() <= salarioMinimo * 4)
+            {
+                Imposto = Convert.ToString(RendaLiquida() * 0.075);
+            }
+            else if (RendaLiquida() <= salarioMinimo * 5)
+            {
+                Imposto = Convert.ToString(RendaLiquida() * 0.15);
+            }
+            else if (RendaLiquida() <= salarioMinimo * 7)
+            {
+                Imposto = Convert.ToString(RendaLiquida() * 0.225);
+            }
+            else
+            {
+                Imposto = Convert.ToString(RendaLiquida() * 0.275);
+            }
+            return Imposto;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Cpf}, Nº dependentes:" +
+                   $"{Dependentes}, renda bruta: R${RendaBruta.ToString("f2")}";
+        }
+    }
+}
---
+++ ImpostoRenda/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpostoRenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deseja cadastrar contribuinte s/S: ");
+            List<Contribuinte> listaContribuintes = new List<Contribuinte>();
+            char resp = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int id = 1;
+
+            while (resp == 's' || resp == 'S')
+            {
+                
+                Console.WriteLine($"ID: {id}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("CPF: ");
+                string cpf = Console.ReadLine();
+                Console.Write("Nº dependentes: ");
+                int dependentes = int.Parse(Console.ReadLine());
+                Console.Write("Renda bruta mensal: ");
+                double rendaBruta = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                listaContribuintes.Add(new Contribuinte(id, nome, cpf, dependentes, rendaBruta));
+
+                Console.Write("Deseja cadastrar novo contribuinte? ");
+                resp = char.Parse(Console.ReadLine());
+                switch(resp)
+                {
+                    case 's':
+                        id++;
+                        break;
+                    case 'S':
+                        id++;
+                        break;
+                }
+               
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            foreach (Contribuinte list in listaContribuintes)
+            {
+                Console.WriteLine(list);
+            }
+            Console.WriteLine();
+
+            Console.Write("Informe o valor do salario minimo atual: ");
+            double salarioMinimo = double.Parse(Console.ReadLine());
+
+            List<Contribuinte> contribuintesImposto = new List<Contribuinte>();
+
+            foreach (Contribuinte contribuinte in listaContribuintes)
+            {
+                
+                contribuinte.RendaLiquida();
+                contribuinte.CalculoIR(salarioMinimo);
+                contribuintesImposto.Add(contribuinte);
+            }
+
+            foreach (Contribuinte contribuinte in contribuintesImposto)
+            {
+                Console.WriteLine($"{contribuinte.Nome}, {contribuinte.Imposto}");
+            }
+
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
